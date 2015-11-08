e0b7148c30f93fde6d7a41382c62d753fec26edc
bencw99 <ben.cohenwang@gmail.com>
2015-11-07 22:15:45 -0800

    created distribution framework

--- src/src/General/Crimes.java
+++ src/src/General/Crimes.java
@@ -10,6 +10,7 @@ public class Crimes {
     public static ArrayList<Crime> crimes;
     public static HashMap<Crime, String> primary_description_map;
     public static String[] primary_descriptions;
+    public static HashMap<Integer, District> district_crimes;
 
 
     public static void mapPrimaryDescriptions() {
@@ -17,5 +18,18 @@ public class Crimes {
         for(Crime c : crimes) {
             primary_description_map.put(c, c.primary_description);
         }
+    }
+    
+    public static void generateDistributions() {
+    	district_crimes = new HashMap<Integer, District>();
+    	for(Crime crime : crimes) {
+    		int district = crime.beat/100;
+    		int beat = crime.beat%100;
+    		if(district_crimes.containsKey(district)) {
+    			district_crimes.put(district, new District());
+        		district_crimes.get(district).set(beat, 0);
+    		}
+    		district_crimes.get(district).set(beat, district_crimes.get(district).get(beat) + 1);
+    	}
     }
 }
---
+++ src/src/General/District.java
@@ -0,0 +1,27 @@
+package General;
+
+import java.util.ArrayList;
+
+public class District {
+	ArrayList<Integer> beats;
+	
+	public District() {
+		beats = new ArrayList<Integer>(100);
+	}
+	
+	public void set(int index, Integer element) {
+		beats.set(index, element);
+	}
+	
+	public Integer get(int index) {
+		return beats.get(index);
+	}
+	
+	public Integer getTotal() {
+		int total = 0;
+		for(Integer integer : beats) {
+			total += integer;
+		}
+		return total;
+	}
+}
